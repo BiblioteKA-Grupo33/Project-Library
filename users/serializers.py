8a566aaec638d33d10855432ea153fc4b7df36c5
bielssinho <bielnmjr@gmail.com>
2023-05-03 13:33:20 -0300

    feat: users app routes and serialazers and views

---
+++ users/serializers.py
@@ -0,0 +1,52 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from .models import User
+from django.contrib.auth.hashers import make_password
+
+
+class UserSerializer(serializers.ModelSerializer):
+    def create(self, validated_data: dict) -> User:
+        if not validated_data["is_employe"]:
+            return User.objects.create_user(**validated_data)
+
+        return User.objects.create_superuser(**validated_data)
+
+    def update(self, instance: User, validated_data: dict) -> User:
+        for key, value in validated_data.items():
+            if key == "password":
+                instance.set_password(value)
+            else:
+                setattr(instance, key, value)
+
+        instance.save()
+
+        return instance
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "password",
+            "is_superuser",
+        ]
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "username": {
+                "validators": [
+                    UniqueValidator(
+                        queryset=User.objects.all(),
+                        message="A user with that username already exists.",
+                    )
+                ]
+            },
+            "email": {
+                "validators": [
+                    UniqueValidator(
+                        queryset=User.objects.all(),
+                        message="A user with that email already exists.",
+                    )
+                ]
+            },
+        }
