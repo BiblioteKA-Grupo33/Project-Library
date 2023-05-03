c2dc623b95056e55eb719d41876aadbb9270719f
bielssinho <bielnmjr@gmail.com>
2023-05-03 14:32:04 -0300

    feat: id with uuid module

--- users/models.py
+++ users/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
 
 
 class User(AbstractUser):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     username = models.CharField(unique=True, max_length=150)
     email = models.EmailField(unique=True, max_length=150)
     password = models.CharField(max_length=150)
